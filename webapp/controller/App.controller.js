492ad6d82b6a3a24e860e9a9fa5c5f98c2d5c417
Lazajo <jose-luis.lazaro@heimbach.com>
2020-11-10 14:48:35 +0000

    Proyecto después de la lección 12
    Change-Id: I69edbc71d1472562ed46bac040b6b7fb06b92a31

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,32 +3,18 @@ sap.ui.define([
 	"sap/m/MessageToast"
 	// "logaligroup/SAPUI5/model/models",
 	// "sap/ui/model/resource/ResourceModel"
-//], function (Controller, MessageToast, models, ResourceModel) {
+	//], function (Controller, MessageToast, models, ResourceModel) {
 ], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("logaligroup.SAPUI5.controller.App", {
 
-		// onInit: function () {
-		// 	//set model on view
-		// 	this.getView().setModel(models.createRecipient());
-			
-		// 	//set i18n model on view
-		// 	var i18nModel = new ResourceModel({
-		// 		bundleName: "logaligroup.SAPUI5.i18n.i18n"
-		// 	});
-		// 	this.getView().setModel(i18nModel, "i18n");
-		// },
+		onInit: function () {
 
-		onShowHello: function () {
-			
-			//read text from i18n and other models
-			var sHello = this.getView().getModel("i18n").getResourceBundle().getText("sayHello");
-			var sName = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = sHello.concat(" ").concat(sName);
-			MessageToast.show(sMsg);
-			
-			//MessageToast.show("Hello World");
+		},
+
+		onOpenHeader: function () {
+			this.getOwnerComponent().openHelloDialog();
 		}
 	});
 });
